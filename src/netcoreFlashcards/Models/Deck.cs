ba72c1e605a0f7d40e8f12696ec209c95c5d5855
BrentLee <brent.lee@extendhealth.com>
2017-07-12 11:16:35 -0600

    minor updates to remove boilerplate html and code.

---
+++ src/netcoreFlashcards/Models/Deck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcoreFlashcards.Models
+{
+    public class Deck
+    {
+        public List<IFlashCard> Cards { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
---
+++ src/netcoreFlashcards/Models/FlashCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcoreFlashcards.Models
+{
+
+    public class FlashCard : IFlashCard
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+    public interface IFlashCard
+    {
+        string Question { get; set; }
+        string Answer { get; set; }
+    }
+}
